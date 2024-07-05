addc0db04e3f725aa9a4da66ad5eaf6ea5c9c1d7
yujin5959 <jinjin17104@naver.com>
2024-07-06 03:47:41 +0900

    프로필 페이지,각연결 페이지 연결 중, 사진 업로드(수정 중), 아이디어 작성 페이지, 날짜 로딩js, 가이드 추가(diary)

--- static/write_idea.js
+++ static/write_idea.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fileInput = document.getElementById("file_upload");
-    const previewContainer = document.getElementById("preview_container");
+document.addEventListener('DOMContentLoaded', function() {
+    var fileInput = document.getElementById('fileInput');
 
-    fileInput.addEventListener("change", function() {
-        const files = fileInput.files;
-        previewContainer.innerHTML = "";
-
-        if (files.length > 4) {
-            alert("최대 4개의 파일만 선택할 수 있습니다.");
-            fileInput.value = "";  // 선택을 초기화
-            return;
-        }
-
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            const reader = new FileReader();
-
+    fileInput.addEventListener('change', function() {
+        var file = this.files[0];
+        if (file) {
+            var reader = new FileReader();
             reader.onload = function(e) {
-                const img = document.createElement("img");
-                img.src = e.target.result;
-                img.className = "preview_image";
-                previewContainer.appendChild(img);
+                var newPreview = document.createElement('div');
+                newPreview.className = 'image-preview';
+                var newImage = document.createElement('img');
+                newImage.src = e.target.result;
+                newImage.alt = '이미지 미리보기';
+                newPreview.appendChild(newImage);
+                document.querySelector('.image_preview_container').appendChild(newPreview);
             };
-
             reader.readAsDataURL(file);
         }
     });
+    
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더합니다.
+    var day = today.getDate();
+
+    var dateString = year + '년 ' + month + '월 ' + day + '일';
+    var h1Element = document.getElementById('write_idea_date');
+    h1Element.textContent = dateString;
 });
